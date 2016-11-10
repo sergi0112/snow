57a7e7d55fe09f3d5064b4b73b2e1bfcb75c6eb7
EiichiSeiji <sergi0112@seiji-no-MacBook-Air.local>
2016-11-10 20:47:49 +0900

    プロジェクト新規作成

---
+++ snow/snow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  snow
+//
+//  Created by seiji eiichi on 2016/11/10.
+//  Copyright © 2016年 sergi0112. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ snow/snow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  snow
+//
+//  Created by seiji eiichi on 2016/11/10.
+//  Copyright © 2016年 sergi0112. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
